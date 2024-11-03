33302f2b8e7f20ffd7bee4782a4ee9ce36a74835
maksimkins <maks.aleshkov.04@gmail.com>
2024-11-03 19:36:49 +0400

    Add Validators

--- academy/controllers/identityController.js
+++ academy/controllers/identityController.js
@@ -10,8 +10,9 @@ const register =  async (req, res) => {
     const { name, surname, email, password } = req.body;
 
     try {
-        const existingUser = await User.findOne({ email });
-        if (existingUser) return res.status(400).json({ message: 'User already exists' });
+        console.log(email)
+        const existingUser = await User.findOne({ where: {email} });
+        if (existingUser) return res.status(400).json({ message: `User with email:${email} already exists` });
 
         const hashedPassword = await bcrypt.hash(password, 10);
         
--- academy/routes/identityRoutes.js
+++ academy/routes/identityRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
+const validatePassword = require('../validators/validatePassword');
+const validateEmail = require('../validators/validateEmail');
 const taskController = require('../controllers/identityController');
 
-router.post('/login', taskController.login);
-router.post('/register', taskController.register);
+router.post('/login', [validateEmail, validatePassword], taskController.login);
+router.post('/register', [validateEmail, validatePassword], taskController.register );
 
 module.exports = router;
